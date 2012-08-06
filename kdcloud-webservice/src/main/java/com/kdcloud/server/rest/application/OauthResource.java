1945af96a86c233157d383e10f5cf2ade39c2f59
vispax <linuspax@gmail.com>
2012-08-06 13:20:46 +0100

    working on oauth

--- kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/OauthResource.java
+++ kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/OauthResource.java
@@ -11,28 +11,35 @@ import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.ClientResource;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+import org.restlet.security.User;
 
 public class OauthResource extends ServerResource {
 	
 
 	@Get
 	public Representation represent() {
-		OAuthUser u = (OAuthUser) getRequest().getClientInfo().getUser();
-	    String token = u.getAccessToken();
+		getLogger().info("new version");
+		User u = getRequest().getClientInfo().getUser();
+		if (u == null)
+			return new StringRepresentation("not authenticated");
+		OAuthUser authenticatedUser = (OAuthUser) u;
+	    String token = authenticatedUser.getAccessToken();
 	    Reference ref = new Reference("https://www.googleapis.com/oauth2/v1/tokeninfo");
 	    ref.addQueryParameter(OAuthServerResource.ACCESS_TOKEN, token);
 	    getLogger().info("validating token: " + ref);
 	    ClientResource client = new ClientResource(ref);
-	    JsonRepresentation rep = (JsonRepresentation) client.get();
-	    try {
-			JSONObject object = rep.getJsonObject();
-			String user = (String) object.get("user_id");
-			return new StringRepresentation("your user_id is " + user);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return new StringRepresentation("error");
-		}
+	    Representation rep = client.get();
+	    if (rep instanceof JsonRepresentation)
+		    try {
+				JSONObject object = ((JsonRepresentation) rep).getJsonObject();
+				String user = (String) object.get("user_id");
+				return new StringRepresentation("your user_id is " + user);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    getLogger().info("not an instance of JsonRepresentation");
+	    return new StringRepresentation("error");
 	}
 
 }

f5c2300ce9a4c94af3a19f291147613a9b2f474b
vispax <linuspax@gmail.com>
2012-08-18 16:44:10 +0100

    fix routing for DeviceResource

--- kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/KDApplication.java
+++ kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/KDApplication.java
@@ -5,10 +5,12 @@ import org.restlet.Restlet;
 import org.restlet.routing.Router;
 
 import com.kdcloud.server.rest.api.DatasetResource;
+import com.kdcloud.server.rest.api.DeviceResource;
 import com.kdcloud.server.rest.api.UserDataResource;
 import com.kdcloud.server.rest.api.ReportResource;
 import com.kdcloud.server.rest.api.SchedulerResource;
 import com.kdcloud.server.rest.resource.DatasetServerResource;
+import com.kdcloud.server.rest.resource.DeviceServerResource;
 import com.kdcloud.server.rest.resource.UserDataServerResource;
 import com.kdcloud.server.rest.resource.ReportServerResource;
 import com.kdcloud.server.rest.resource.SchedulerServerResource;
@@ -28,6 +30,7 @@ public class KDApplication extends Application {
 		router.attach(DatasetResource.URI, DatasetServerResource.class);
 		router.attach(SchedulerResource.URI, SchedulerServerResource.class);
 		router.attach(ReportResource.URI, ReportServerResource.class);
+		router.attach(DeviceResource.URI, DeviceServerResource.class);
 		
 		return router;
 	}

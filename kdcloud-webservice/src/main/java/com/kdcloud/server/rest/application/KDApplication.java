e79e8dffd28b864829f807b406b05135063e7841
vispax <linuspax@gmail.com>
2012-08-29 16:29:36 +0100

    KDApplication now uses reflection for mapping

--- kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/KDApplication.java
+++ kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/KDApplication.java
@@ -1,38 +1,18 @@
 package com.kdcloud.server.rest.application;
 
+import java.util.Set;
+
 import javax.jdo.PersistenceManager;
 
+import org.reflections.Reflections;
 import org.restlet.Application;
 import org.restlet.Restlet;
 import org.restlet.routing.Router;
 
 import com.kdcloud.server.dao.ModalityDao;
-import com.kdcloud.server.entity.Modality;
-import com.kdcloud.server.entity.ServerAction;
-import com.kdcloud.server.entity.ServerMethod;
-import com.kdcloud.server.entity.ServerParameter;
 import com.kdcloud.server.jdo.GaeModalityDao;
 import com.kdcloud.server.jdo.PMF;
-import com.kdcloud.server.rest.api.AnalysisResource;
-import com.kdcloud.server.rest.api.DatasetResource;
-import com.kdcloud.server.rest.api.DeviceResource;
-import com.kdcloud.server.rest.api.GlobalAnalysisResource;
-import com.kdcloud.server.rest.api.GlobalDataResource;
-import com.kdcloud.server.rest.api.ModalitiesResource;
-import com.kdcloud.server.rest.api.ReportResource;
-import com.kdcloud.server.rest.api.SchedulerResource;
-import com.kdcloud.server.rest.api.UserDataResource;
-import com.kdcloud.server.rest.api.UserDetailsResource;
-import com.kdcloud.server.rest.resource.AnalysisServerResource;
-import com.kdcloud.server.rest.resource.DatasetServerResource;
-import com.kdcloud.server.rest.resource.DeviceServerResource;
-import com.kdcloud.server.rest.resource.GlobalAnalysisServerResource;
-import com.kdcloud.server.rest.resource.GlobalDataServerResource;
-import com.kdcloud.server.rest.resource.ModalitiesServerResource;
-import com.kdcloud.server.rest.resource.ReportServerResource;
-import com.kdcloud.server.rest.resource.SchedulerServerResource;
-import com.kdcloud.server.rest.resource.UserDataServerResource;
-import com.kdcloud.server.rest.resource.UserDetailsServerResource;
+import com.kdcloud.server.rest.resource.KDServerResource;
 
 public class KDApplication extends Application {
 
@@ -41,7 +21,7 @@ public class KDApplication extends Application {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		ModalityDao modalityDao = new GaeModalityDao(pm);
 		if (modalityDao.getAll().isEmpty()) {
-			addStandardModalities(modalityDao);
+			Utils.addStandardModalities(modalityDao);
 		}
 	}
 
@@ -53,46 +33,36 @@ public class KDApplication extends Application {
 
 		Router router = new Router(getContext());
 
-		router.attach(UserDataResource.URI, UserDataServerResource.class);
-		router.attach(DatasetResource.URI, DatasetServerResource.class);
-		router.attach(AnalysisResource.URI, AnalysisServerResource.class);
-		router.attach(SchedulerResource.URI, SchedulerServerResource.class);
-		router.attach(ReportResource.URI, ReportServerResource.class);
-		router.attach(DeviceResource.URI, DeviceServerResource.class);
-		router.attach(UserDetailsResource.URI, UserDetailsServerResource.class);
-		router.attach(ModalitiesResource.URI, ModalitiesServerResource.class);
-		router.attach(GlobalAnalysisResource.URI,
-				GlobalAnalysisServerResource.class);
-		router.attach(GlobalDataResource.URI, GlobalDataServerResource.class);
+		Reflections reflections = new Reflections(
+				"com.kdcloud.server.rest.resource");
+
+		Set<Class<? extends KDServerResource>> allClasses = reflections
+				.getSubTypesOf(KDServerResource.class);
+
+		for (Class<? extends KDServerResource> clazz : allClasses) {
+			System.out.println(clazz.getSimpleName());
+			try {
+				String uri = clazz.getField("URI").get(null).toString();
+				router.attach(uri, clazz);
+				getLogger().info("mapped uri " + uri);
+			} catch (Exception e) {
+
+			}
+		}
+
+//		router.attach(UserDataResource.URI, UserDataServerResource.class);
+//		router.attach(DatasetResource.URI, DatasetServerResource.class);
+//		router.attach(AnalysisResource.URI, AnalysisServerResource.class);
+//		router.attach(SchedulerResource.URI, SchedulerServerResource.class);
+//		router.attach(ReportResource.URI, ReportServerResource.class);
+//		router.attach(DeviceResource.URI, DeviceServerResource.class);
+//		router.attach(UserDetailsResource.URI, UserDetailsServerResource.class);
+//		router.attach(ModalitiesResource.URI, ModalitiesServerResource.class);
+//		router.attach(GlobalAnalysisResource.URI,
+//				GlobalAnalysisServerResource.class);
+//		router.attach(GlobalDataResource.URI, GlobalDataServerResource.class);
 
 		return router;
 	}
-	
-	public static void addStandardModalities(ModalityDao modalityDao) {
-		Modality dataFeed = new Modality();
-		dataFeed.setName("Data Feed");
-		dataFeed.getSensors().add("ecg");
-		ServerAction createDataset = new ServerAction(UserDataResource.URI,
-				ServerParameter.DATASET_ID.getName(), ServerMethod.GET, false,
-				10 * 60 * 1000);
-		dataFeed.getServerCommands().add(createDataset);
-		ServerAction uploadData = new ServerAction(DatasetResource.URI, null,
-				ServerMethod.PUT, true, 10 * 60 * 1000);
-		dataFeed.getServerCommands().add(uploadData);
-		modalityDao.save(dataFeed);
 
-		Modality singleAnalysis = new Modality();
-		singleAnalysis.setName("Single Analysis");
-		ServerAction analyze = new ServerAction(AnalysisResource.URI, null,
-				ServerMethod.GET, false, 0);
-		singleAnalysis.getServerCommands().add(analyze);
-		modalityDao.save(singleAnalysis);
-
-		Modality globalAnalysis = new Modality();
-		globalAnalysis.setName("Global Analysis");
-		ServerAction globalAnalyze = new ServerAction(
-				GlobalAnalysisResource.URI, null, ServerMethod.GET, false, 0);
-		globalAnalysis.getServerCommands().add(globalAnalyze);
-		modalityDao.save(globalAnalysis);
-	}
 }
---
+++ kdcloud-webservice/src/test/java/com/kdcloud/server/rest/application/TestReflection.java
@@ -0,0 +1,11 @@
+package com.kdcloud.server.rest.application;
+
+
+public class TestReflection {
+
+//	@Test
+	public void test() {
+		
+	}
+
+}

425bc9a5b1a77eb8b2a3dae779900a9debd860ca
vispax <linuspax@gmail.com>
2012-10-26 23:22:17 +0200

    added license

--- kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/MainApplication.java
+++ kdcloud-webservice/src/main/java/com/kdcloud/server/rest/application/MainApplication.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2012 Vincenzo Pirrone
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package com.kdcloud.server.rest.application;
 
 import java.util.logging.Level;
@@ -7,7 +23,6 @@ import org.restlet.Context;
 import org.restlet.Restlet;
 import org.restlet.data.ChallengeScheme;
 import org.restlet.resource.Directory;
-import org.restlet.routing.Filter;
 import org.restlet.routing.Router;
 import org.restlet.security.ChallengeAuthenticator;
 
@@ -18,15 +33,14 @@ public class MainApplication extends Application {
 	
 	@Override
 	public Restlet createInboundRoot() {
-		getLogger().setLevel(Level.FINEST);
+		getLogger().setLevel(Level.INFO);
 		
 		Context applicationContext = new GAEContext(getLogger());
 
 		Router router = new Router(getContext());
 		
 		router.attach("/api", new Directory(getContext(), "war:///"));
-		new Filter() {
-		};
+		
 		
 		router.attach(TaskQueue.WORKER_URI, QueueWorkerServerResource.class);
 

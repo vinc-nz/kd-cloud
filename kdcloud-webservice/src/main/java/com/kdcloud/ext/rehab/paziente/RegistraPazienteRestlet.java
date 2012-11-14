f612c4a46202f74cbcc76d4bd8ecf6a7061e0592
fabrizio1987 <fabrizio.granieri@gmail.com>
2012-11-14 22:22:49 +0100

    prova get e post

--- kdcloud-webservice/src/main/java/com/kdcloud/ext/rehab/paziente/RegistraPazienteRestlet.java
+++ kdcloud-webservice/src/main/java/com/kdcloud/ext/rehab/paziente/RegistraPazienteRestlet.java
@@ -3,9 +3,13 @@ package com.kdcloud.ext.rehab.paziente;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.restlet.data.MediaType;
 import org.restlet.ext.xml.DomRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -18,54 +22,69 @@ import com.kdcloud.server.rest.resource.KDServerResource;
 public class RegistraPazienteRestlet extends KDServerResource {
 
 	public static final String URI = "/rehab/registrapaziente";
+	
+//	@Post("xml")
+//	DomRepresentation acceptReturnXml(Representation entity) { accept(entity); return .. }
 
-	@Post
-	protected DomRepresentation doPost(DomRepresentation d) {
+	@Post("xml")
+	public DomRepresentation doPost(DomRepresentation d) {
 
 		// User user = getUser();
 		// String username = user.getName();
+
 		
 		Document doc = null;
-		try {
-			doc = d.getDocument();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		// handle document input
-		Element rootEl = doc.getDocumentElement();
-		String username = XMLUtils.getTextValue(rootEl, "username");
-		String password = XMLUtils.getTextValue(rootEl, "password");
-		String nome = XMLUtils.getTextValue(rootEl, "nome");
-		String cognome = XMLUtils.getTextValue(rootEl, "cognome");
-
-		String esito = "";
-		try {
-			ObjectifyService.register(Paziente.class);
-		} catch (Exception e) {
-		}
-
-		Objectify ofy = ObjectifyService.begin();
-		Paziente paz = ofy.query(Paziente.class).filter("username", username)
-				.get();
-		if (paz != null) {
-			esito = "errore";
-		} else {
-
-			Paziente paziente = new Paziente(username, nome, cognome);
-			ofy.put(paziente);
-			esito = "OK";
-		}
-
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("esito", esito);
-		Document ris = XMLUtils.createXMLResult("registrapazienteOutput", map);
 		
-		DomRepresentation result = new DomRepresentation(MediaType.TEXT_XML, ris);
-		
-		return result;
+			try {
+				doc = d.getDocument();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				
+			}
 
+			// handle document input
+			Element rootEl = doc.getDocumentElement();
+			String username = XMLUtils.getTextValue(rootEl, "username");
+			String password = XMLUtils.getTextValue(rootEl, "password");
+			String nome = XMLUtils.getTextValue(rootEl, "nome");
+			String cognome = XMLUtils.getTextValue(rootEl, "cognome");
+
+			String esito = "";
+			try {
+				ObjectifyService.register(Paziente.class);
+			} catch (Exception e) {
+			}
+
+			Objectify ofy = ObjectifyService.begin();
+			Paziente paz = ofy.query(Paziente.class)
+					.filter("username", username).get();
+			if (paz != null) {
+				esito = "errore";
+			} else {
+
+				Paziente paziente = new Paziente(username, nome, cognome);
+				ofy.put(paziente);
+				esito = "OK";
+			}
+
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("esito", esito);
+			Document ris = XMLUtils.createXMLResult("registrapazienteOutput",
+					map);
+
+			DomRepresentation result = new DomRepresentation(
+					MediaType.TEXT_XML, ris);
+
+			return result;
+		
+
+	}
+	
+	@Get
+	public String toString(){
+		return "ciao";
+		
 	}
 
 }

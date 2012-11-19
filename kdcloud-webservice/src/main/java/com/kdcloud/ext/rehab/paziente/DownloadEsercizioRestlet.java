b2b67bce8741fc5e51d15291843673480a55aa91
fabrizio1987 <fabrizio.granieri@gmail.com>
2012-11-19 23:28:03 +0100

    download esercizio - da controllare

--- kdcloud-webservice/src/main/java/com/kdcloud/ext/rehab/paziente/DownloadEsercizioRestlet.java
+++ kdcloud-webservice/src/main/java/com/kdcloud/ext/rehab/paziente/DownloadEsercizioRestlet.java
@@ -1,11 +1,11 @@
 package com.kdcloud.ext.rehab.paziente;
 
+import java.util.List;
+
 import org.restlet.data.MediaType;
-import org.restlet.data.Status;
 import org.restlet.ext.xml.DomRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Post;
-import org.restlet.resource.ResourceException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -14,10 +14,8 @@ import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
 import com.kdcloud.ext.rehab.db.EsercizioRiferimento;
 import com.kdcloud.ext.rehab.db.Paziente;
-import com.kdcloud.server.entity.User;
-import com.kdcloud.server.rest.resource.KDServerResource;
 
-public class DownloadEsercizioRestlet extends RehabServerResource { //KDServerResource{ //
+public class DownloadEsercizioRestlet extends RehabServerResource { 
 
 	public static final String URI = "/rehab/downloadesercizio";
 
@@ -41,8 +39,6 @@ public class DownloadEsercizioRestlet extends RehabServerResource { //KDServerRe
 			d = getEsercizioByPazienteAndNumero(username, esercizio, d);
 
 		} catch (Exception e) {
-			// ritorna l'xml di errore col messaggio
-			// result = errore..
 			result = XMLUtils.createXMLError("errore download esercizio", ""
 					+ e.getMessage());
 		}
@@ -53,18 +49,20 @@ public class DownloadEsercizioRestlet extends RehabServerResource { //KDServerRe
 	private Document getEsercizioByPazienteAndNumero(String username,
 			int esercizio, Document doc) throws Exception {
 
-//		Key<Paziente> paz = new Key<Paziente>(Paziente.class, username);
-//
-//		try {
-//			ObjectifyService.register(EsercizioRiferimento.class);
-//		} catch (Exception e) {
-//
-//		}
-//
-//		Objectify ofy = ObjectifyService.begin();
-//		List<EsercizioRiferimento> list = ofy.query(EsercizioRiferimento.class)
-//				.order("timestamp").filter("paziente", paz)
-//				.filter("numero", esercizio).list();
+		Key<Paziente> paz = new Key<Paziente>(Paziente.class, username);
+
+		try {
+			ObjectifyService.register(EsercizioRiferimento.class);
+		} catch (Exception e) {
+
+		}
+
+		Objectify ofy = ObjectifyService.begin();
+		//List<EsercizioRiferimento> list = ofy.query(EsercizioRiferimento.class).list();
+				//.filter("numero", esercizio).filter("paziente", paz)
+		//.order("timestamp")
+		EsercizioRiferimento es = ofy.query(EsercizioRiferimento.class).filter("numero", esercizio)
+				.get();
 		
 		Element root = doc.createElement("downloadesercizioOutput");
 		root.setAttribute("numero", "" + esercizio);
@@ -72,30 +70,30 @@ public class DownloadEsercizioRestlet extends RehabServerResource { //KDServerRe
 		doc.appendChild(root);
 
 //		if (list != null & !list.isEmpty()) {
-//
-//			
-//
-//			for (EsercizioRiferimento e : list) {
-//				int[] raw = e.getRaw();
-//				int[] angles = e.getAngoli();
-//				Element child = doc.createElement("esercizio");
-//
-//				child.setAttribute("timestamp", "" + e.getTimestamp());
-//				child.setAttribute("raw_bx", "" + raw[0]);
-//				child.setAttribute("raw_by", "" + raw[1]);
-//				child.setAttribute("raw_bz", "" + raw[2]);
-//				child.setAttribute("raw_fx", "" + raw[3]);
-//				child.setAttribute("raw_fy", "" + raw[4]);
-//				child.setAttribute("raw_fz", "" + raw[5]);
-//				child.setAttribute("elbowknee", "" + angles[0]);
-//				child.setAttribute("backline", "" + angles[1]);
-//				child.setAttribute("foreline", "" + angles[2]);
-//				child.setAttribute("sideangle", "" + angles[3]);
-//
-//				root.appendChild(child);
-//
+
+			
+
+//			for (EsercizioRiferimento es : list) {
+				int[] raw = es.getRaw();
+				int[] angles = es.getAngoli();
+				Element child = doc.createElement("esercizio");
+
+				child.setAttribute("timestamp", "" + es.getTimestamp());
+				child.setAttribute("raw_bx", "" + raw[0]);
+				child.setAttribute("raw_by", "" + raw[1]);
+				child.setAttribute("raw_bz", "" + raw[2]);
+				child.setAttribute("raw_fx", "" + raw[3]);
+				child.setAttribute("raw_fy", "" + raw[4]);
+				child.setAttribute("raw_fz", "" + raw[5]);
+				child.setAttribute("elbowknee", "" + angles[0]);
+				child.setAttribute("backline", "" + angles[1]);
+				child.setAttribute("foreline", "" + angles[2]);
+				child.setAttribute("sideangle", "" + angles[3]);
+
+				root.appendChild(child);
+
 //			}
-//
+
 //		}
 		return doc;
 

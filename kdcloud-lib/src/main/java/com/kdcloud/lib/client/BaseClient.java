9265ef46bffba9636739ec85594573c45ab3a3f2
sp4x <linuspax@gmail.com>
2014-03-15 17:58:09 +0100

    updated license info

--- kdcloud-lib/src/main/java/com/kdcloud/lib/client/BaseClient.java
+++ kdcloud-lib/src/main/java/com/kdcloud/lib/client/BaseClient.java
@@ -1,16 +1,17 @@
 /**
- * Copyright (C) 2012 Vincenzo Pirrone
+ * Copyright (C) 2012, Vincenzo Pirrone <pirrone.v@gmail.com>
+
  * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
+ * the terms of the GNU Affero General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with
+ * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
@@ -193,7 +194,9 @@ public abstract class BaseClient implements Runnable {
 
 	public List<ModalitySpecification> getModalities() {
 		setResourceReference("/modality");
+		
 		LinkedList<ModalitySpecification> list = new LinkedList<ModalitySpecification>();
+		
 		Index modalityIndex = resource.wrap(IndexResource.class).buildIndex();
 		for (String uri : modalityIndex.getAllReferences()) {
 			resource.setReference(uri);
